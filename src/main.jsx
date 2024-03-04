c6a4a2ea3cabcdd4aa4fac926e9e33aa27c3a567
ireaneus <bwdave@protonmail.com>
2024-03-04 12:41:36 -0700

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import Blog from './Blog';
+import theme from './theme';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <Blog />
+    </ThemeProvider>
+    ,
+  </React.StrictMode>
+);
